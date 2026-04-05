d9ae990411efe7c6d929879a2f06d9fb34dd5c03
bhumikakadu97-byte <bhumikakadu97@gmail.com>
2026-04-05 11:36:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -40,4 +40,19 @@ document.body.style.overflowY = 'scroll';
 
 
 
+  document.getElementById("exploreBtn1").addEventListener("click", function() {
+  window.location.href = "uiux.html";
+});
+
+const goBackBtn = document.getElementById("goBackBtn");
+const msite = document.getElementById("msite");
+
+goBackBtn.addEventListener("click", function() {
+    msite.scrollIntoView({ behavior: "smooth" }); // Sirf Main Site ke top par scroll
+});
+
+document.body.style.overflowY = 'scroll';
+
+
+
 
